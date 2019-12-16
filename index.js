e8335bf0600a8056bdbae9693efba0261e9ee4f1
Simon Moe <simmoe@gmail.com>
2019-12-16 08:46:26 +0100

    tried but gave up github

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const status = document.querySelector('#status p')
 
     auth.onAuthStateChanged((user) => {
+        console.log('auth state', user)
         if (user) {
-            status.innerHTML = 
+            status.innerHTML =
                 `<h4>logged in</h4>
                 <p>${user.email}</p>
                 `
-                console.log(user)
-
         } else {
             status.innerHTML = 'user logged out'
         }
@@ -22,13 +21,45 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const signUpPass = document.querySelector('#signup-email-password input[type=password]')
 
     signUpWithEmailButton.addEventListener('click', () => {
-        if(!signUpEmail.checkValidity()){
-            signUpEmail.placeholder = 'please fill in a correct email'
-            return
-        }
-        auth.signUpWithEmailAndPassword(signUpEmail, signUpPass)
-        
+        if (!signUpEmail.checkValidity()) signUpEmail.style.border = '1px dashed orange'
+        if (!signUpPass.checkValidity()) signUpPass.style.border = '1px dashed orange'
+        if (signUpEmail.checkValidity() && signUpPass.checkValidity())
+            auth.createUserWithEmailAndPassword(signUpEmail.value, signUpPass.value)
+            .then(() => {
+                signUpEmail.value = ''
+                signUpPass.value = ''
+            })
+            .catch(function (error) {
+                status.innerHTML =
+                    `<h4>${error.code}</h4>
+                <p>${error.message}</p>
+                `
+            })
     })
+
+    //sign IN with email/password
+    const signInWithEmailButton = document.querySelector('#login-email-password button')
+    const loginEmail = document.querySelector('#login-email-password input[type=email]')
+    const loginPass = document.querySelector('#login-email-password input[type=password]')
+
+    signInWithEmailButton.addEventListener('click', () => {
+        if (!loginEmail.checkValidity()) loginEmail.style.border = '1px dashed orange'
+        if (!loginPass.checkValidity()) loginPass.style.border = '1px dashed orange'
+        if (loginEmail.checkValidity() && loginPass.checkValidity())
+            auth.signInWithEmailAndPassword(loginEmail.value, loginPass.value)
+            .then(() => {
+                loginEmail.value = ''
+                loginPass.value = ''
+            })
+            .catch(function (error) {
+                status.innerHTML =
+                    `<h4>${error.code}</h4>
+                <p>${error.message}</p>
+                `
+            })
+    })
+
+
     //Google auth example
     const signInWithGoogleButton = document.querySelector('#sign-in-with-google button')
 
@@ -39,7 +70,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
         provider.addScope('profile')
         provider.addScope('email')
-        firebase.auth().signInWithRedirect(provider);
+
+        auth.signInWithRedirect(provider)
+            .catch(function (error) {
+                status.innerHTML =
+                    `<h4>${error.code}</h4>
+                  <p>${error.message}</p>
+                `
+            })
     })
 
     //sign out
